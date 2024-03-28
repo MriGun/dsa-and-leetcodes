8ac2501ee71413be3d39b96246335d4e9cb3c4d3
mrinmoybm <eDr45gSc24#>
2024-03-28 09:02:10 +0600

    pattern

--- src/main/java/com/mricode/leetcode/dsa/pattern/Pattern.java
+++ src/main/java/com/mricode/leetcode/dsa/pattern/Pattern.java
@@ -2,7 +2,7 @@ package com.mricode.leetcode.dsa.pattern;
 
 public class Pattern {
     public static void main(String[] args) {
-        pattern30(5);
+        pattern31(5);
     }
 
     static void pattern1(int n) {
@@ -69,6 +69,25 @@ public class Pattern {
         }
     }
 
+    static void pattern17(int n) {
+        for (int row = 1; row <= 2* n; row++) {
+
+            int totalColsInRow = row > n ? 2*n -row : row;
+            for (int space = 0; space < n-totalColsInRow ; space++) {
+                System.out.print(" ");
+            }
+
+            for (int col = totalColsInRow; col >= 1; col--) {
+                System.out.print(col);
+            }
+
+            for (int col = 2; col <= totalColsInRow; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
     static void pattern28(int n) {
         for (int row=0; row<2*n; row++) {
             int totalColsInRow = row > n ? 2*n -row : row;
@@ -97,6 +116,20 @@ public class Pattern {
 
             for (int col = 2; col <= row; col++) {
                 System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+    
+    static void pattern31(int n) {
+        int originalN = n;
+        n = 2 * n;
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+
+                //int atEveryIndex = Math.min(Math.min(row, col), Math.min(n-row, n-col));
+                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n-row, n-col));
+                System.out.print(atEveryIndex + " ");
             }
             System.out.println();
         }
