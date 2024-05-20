10d0512984b96970775dd439db9edb083d0e23a1
mrinmoybm <eDr45gSc24#>
2024-05-20 09:24:04 +0600

    recursion

--- src/main/java/com/mricode/leetcode/dsa/recursion/strings/Stream.java
+++ src/main/java/com/mricode/leetcode/dsa/recursion/strings/Stream.java
@@ -4,6 +4,8 @@ public class Stream {
     public static void main(String[] args) {
         //skip("", "baccadh");
         System.out.println(skip("baccadh"));
+        System.out.println(skipApple("baccappleyg"));
+        System.out.println(skipAppNotApple("baccappeyg"));
     }
 
     static void skip(String processed, String up) {
@@ -37,6 +39,40 @@ public class Stream {
         }
         else {
             return  ch + skip(up.substring(1));
+        }
+
+    }
+
+    static String skipApple(String up) {
+
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        char ch = up.charAt(0);
+
+        if (up.startsWith("apple")) {
+            return skipApple(up.substring(5));
+        }
+        else {
+            return  ch + skipApple(up.substring(1));
+        }
+
+    }
+
+    static String skipAppNotApple(String up) {
+
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        char ch = up.charAt(0);
+
+        if (up.startsWith("app") && !up.startsWith("apple")) {
+            return skipAppNotApple(up.substring(3));
+        }
+        else {
+            return  ch + skipAppNotApple(up.substring(1));
         }
 
     }
