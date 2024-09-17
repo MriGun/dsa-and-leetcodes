9af789328957da356909799b690ef772bdd32326
mrinmoybm <eDr45gSc24#>
2024-09-17 09:14:05 +0600

    circular linkedlist

--- src/main/java/com/mricode/leetcode/dsa/linkedlist/SingleLinkedList.java
+++ src/main/java/com/mricode/leetcode/dsa/linkedlist/SingleLinkedList.java
@@ -61,6 +61,22 @@ public class SingleLinkedList {
 
     }
 
+    //insert using recursion
+    public void insertRecursion(int val, int index) {
+        head = insertRec(val, index, head);
+    }
+
+    private Node insertRec(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+
+        node.next = insertRec(val, index-1, node.next);
+        return node;
+    }
+
     public void display() {
         Node temp = head;
         while (temp != null) {
