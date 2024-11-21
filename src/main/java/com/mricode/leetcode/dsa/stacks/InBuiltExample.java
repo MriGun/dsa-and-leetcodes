c59d015d38faaa40b32f1d2fd826301ac4ae4e13
mrinmoybm <eDr45gSc24#>
2024-11-21 09:14:32 +0600

    stacks and queue

---
+++ src/main/java/com/mricode/leetcode/dsa/stacks/InBuiltExample.java
@@ -0,0 +1,39 @@
+package com.mricode.leetcode.dsa.stacks;
+
+import java.util.*;
+
+public class InBuiltExample {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(11);
+        stack.push(21);
+        stack.push(31);
+        stack.push(15);
+        stack.push(9);
+        stack.push(29);
+
+        /*System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());*/
+
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(3);
+        queue.add(6);
+        queue.add(5);
+        queue.add(9);
+        queue.add(15);
+        queue.add(1);
+
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+    }
+}
