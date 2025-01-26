7998792ff7b5e3d867ed2b71f4d25c8a8e9a0f25
mrinmoybm <eDr45gSc24#>
2025-01-26 09:19:52 +0600

    implement binary search tree

--- src/main/java/com/mricode/leetcode/dsa/tree/BinarySearchTree.java
+++ src/main/java/com/mricode/leetcode/dsa/tree/BinarySearchTree.java
@@ -65,8 +65,56 @@ public class BinarySearchTree {
             return true;
         }
 
-        return Math.abs(height(node.left) - height(node.right)) <=1;
+        return Math.abs(height(node.left) - height(node.right)) <=1 && balanced(node.left) && balanced(node.right);
 
+    }
+
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
+        }
+    }
+
+    public void populatedSorted(int[] nums) {
+        populatedSorted(nums, 0, nums.length);
+    }
+
+    private void populatedSorted(int[] nums, int start, int end) {
+
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        this.insert(nums[mid]);
+        populatedSorted(nums, start, mid);
+        populatedSorted(nums, mid + 1, end);
+
+    }
+
+    public void displayWithHeight() {
+        display(this.root, "Root Node : ");
+    }
+
+    private void displayWithHeight(NodeHeight node, String details) {
+
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(details + node.val);
+        displayWithHeight(node.left, "Left child of " + node.val + " : ");
+        displayWithHeight(node.right, "Right child of " + node.val + " : ");
+
+    }
+
+    public static void main(String[] args) {
+
+        BinarySearchTree bst = new BinarySearchTree();
+        int[] nums = {5, 2, 7, 1, 4, 6, 9, 8, 3, 10};
+        bst.populate(nums);
+        bst.display();
     }
 
 }
