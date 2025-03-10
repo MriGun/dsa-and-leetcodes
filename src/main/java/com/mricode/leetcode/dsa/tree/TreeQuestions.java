29fdee9cb91e65f2e6362fea6a0cf9a104fcf2cc
mrinmoybm <eDr45gSc24#>
2025-03-10 22:10:32 +0600

    tree questons

---
+++ src/main/java/com/mricode/leetcode/dsa/tree/TreeQuestions.java
@@ -0,0 +1,48 @@
+package com.mricode.leetcode.dsa.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class TreeQuestions {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevelList = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+
+                 TreeNode currentNode = queue.poll();
+                 currentLevelList.add(currentNode.val);
+                 if (currentNode.left != null) {
+                     queue.offer(currentNode.left);
+                 }
+
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentLevelList);
+        }
+
+        return result;
+
+    }
+}
