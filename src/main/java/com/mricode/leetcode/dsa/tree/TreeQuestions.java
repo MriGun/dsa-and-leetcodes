7b83cc2af7c54e921148bed2c5a3164509ae1395
mrinmoybm <eDr45gSc24#>
2025-03-12 22:58:04 +0600

    tree questons

--- src/main/java/com/mricode/leetcode/dsa/tree/TreeQuestions.java
+++ src/main/java/com/mricode/leetcode/dsa/tree/TreeQuestions.java
@@ -156,4 +156,37 @@ public class TreeQuestions {
          }
          return result;
      }
+
+
+     //https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+         List<List<Integer>> result = new ArrayList<>();
+
+         if (root == null) {
+             return result;
+         }
+
+         Queue<TreeNode> queue = new LinkedList<>();
+         queue.offer(root);
+
+         while (!queue.isEmpty()) {
+             int levelSize = queue.size();
+             List<Integer> currentLevelList = new ArrayList<>(levelSize);
+             for (int i = 0; i < levelSize; i++) {
+
+                 TreeNode currentNode = queue.poll();
+                 currentLevelList.add(currentNode.val);
+                 if (currentNode.left != null) {
+                     queue.offer(currentNode.left);
+                 }
+
+                 if (currentNode.right != null) {
+                     queue.offer(currentNode.right);
+                 }
+             }
+             result.add(0, currentLevelList);
+         }
+
+         return result;
+     }
 }
