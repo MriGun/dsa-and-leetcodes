827b47c6ceafe42544f0f019ab1cb62499334d5c
mrinmoybm <eDr45gSc24#>
2025-03-14 14:58:16 +0600

    tree questons

--- src/main/java/com/mricode/leetcode/dsa/tree/TreeQuestions.java
+++ src/main/java/com/mricode/leetcode/dsa/tree/TreeQuestions.java
@@ -225,4 +225,36 @@ public class TreeQuestions {
         }
         return node;
      }
+
+     //https://leetcode.com/problems/binary-tree-right-side-view/description/
+     public List<Integer> rightSideView(TreeNode root) {
+         List<Integer> result = new ArrayList<>();
+
+         if (root == null) {
+             return result;
+         }
+
+         Queue<TreeNode> queue = new LinkedList<>();
+         queue.offer(root);
+
+         while (!queue.isEmpty()) {
+             int levelSize = queue.size();
+             List<Integer> currentLevelList = new ArrayList<>(levelSize);
+             for (int i = 0; i < levelSize; i++) {
+
+                 TreeNode currentNode = queue.poll();
+                 currentLevelList.add(currentNode.val);
+                 if (currentNode.left != null) {
+                     queue.offer(currentNode.left);
+                 }
+
+                 if (currentNode.right != null) {
+                     queue.offer(currentNode.right);
+                 }
+             }
+             result.add(currentLevelList.get(levelSize-1));
+         }
+
+         return result;
+     }
 }
