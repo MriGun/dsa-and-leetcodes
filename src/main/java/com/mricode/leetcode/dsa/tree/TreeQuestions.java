99c5b2f82f8bc72542210254c7e672155e842324
mrinmoybm <eDr45gSc24#>
2025-03-15 14:50:44 +0600

    tree questons bfs

--- src/main/java/com/mricode/leetcode/dsa/tree/TreeQuestions.java
+++ src/main/java/com/mricode/leetcode/dsa/tree/TreeQuestions.java
@@ -316,4 +316,37 @@ public class TreeQuestions {
          return level(node.right, x, level+1);
 
     }
+
+    //https://leetcode.com/problems/symmetric-tree/description/
+    public boolean isSymmetric(TreeNode root) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()) {
+
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
+
+            if (left.val != right.val) {
+                return false;
+            }
+
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+
+        }
+
+        return true;
+    }
 }
