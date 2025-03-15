801053544ebe36ee89c04b7310fee1cbbdcaf9c7
mrinmoybm <eDr45gSc24#>
2025-03-15 13:03:02 +0600

    tree questons

--- src/main/java/com/mricode/leetcode/dsa/tree/TreeQuestions.java
+++ src/main/java/com/mricode/leetcode/dsa/tree/TreeQuestions.java
@@ -257,4 +257,63 @@ public class TreeQuestions {
 
          return result;
      }
+
+
+     //https://leetcode.com/problems/cousins-in-binary-tree/
+    public boolean isCousins(TreeNode root, int x, int y) {
+          TreeNode xx = findNode(root, x);
+          TreeNode yy = findNode(root, y);
+
+          return (
+                  (level(root, xx, 0) == level(root, yy, 0)) && (!isSiblings(root, xx, yy))
+          );
+    }
+
+    TreeNode findNode(TreeNode node, int x) {
+
+        if (node == null) {
+            return null;
+        }
+
+        if (node.val == x) {
+            return node;
+        }
+
+        TreeNode n = findNode(node.left, x);
+        if (n != null) {
+            return n;
+        }
+
+        return findNode(node.right, x);
+    }
+
+    boolean isSiblings(TreeNode node, TreeNode x, TreeNode y) {
+
+        if (node == null) {
+            return false;
+        }
+
+        return (
+                (node.left == x && node.right == y) || (node.left == y && node.right == x) || isSiblings(node.left, x, y) || isSiblings(node.right, x, y)
+        );
+    }
+
+    private int level(TreeNode node, TreeNode x, int level) {
+         if (node == null) {
+             return 0;
+         }
+
+         if (node == x) {
+            return level;
+         }
+
+         int l = level(node.left, x, level+1);
+
+         if (l != 0) {
+             return l;
+         }
+
+         return level(node.right, x, level+1);
+
+    }
 }
