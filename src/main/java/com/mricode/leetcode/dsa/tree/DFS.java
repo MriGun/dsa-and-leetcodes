a234e2a09d92cc74e4eecc2809a6d05bb2d63672
mrinmoybm <eDr45gSc24#>
2025-03-16 08:27:41 +0600

    tree questons dfs

---
+++ src/main/java/com/mricode/leetcode/dsa/tree/DFS.java
@@ -0,0 +1,25 @@
+package com.mricode.leetcode.dsa.tree;
+
+import com.mricode.leetcode.dsa.tree.structure.TreeNode;
+
+public class DFS {
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter -1;
+    }
+
+    int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        int dia = leftHeight + rightHeight + 1;
+        diameter = Math.max(diameter, dia);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
