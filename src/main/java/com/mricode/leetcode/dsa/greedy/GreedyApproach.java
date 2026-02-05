b1169ae8bd9f9256187d7987c06cae5faa1f16f1
mrinmoybm <eDr45gSc24#>
2026-02-05 09:36:18 +0600

    greedy approach

---
+++ src/main/java/com/mricode/leetcode/dsa/greedy/GreedyApproach.java
@@ -0,0 +1,33 @@
+package com.mricode.leetcode.dsa.greedy;
+
+import java.util.Arrays;
+
+public class GreedyApproach {
+
+    //https://www.geeksforgeeks.org/problems/activity-selection-1587115620/1
+    public int activitySelection(int[] start, int[] finish) {
+        // code here
+
+        int n = start.length;
+        int[][] interval = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            interval[i][0] = start[i];
+            interval[i][1] = finish[i];
+        }
+
+        Arrays.sort(interval, (a,b) -> a[1] - b[1]);
+
+        int activityCount = 1;
+        int lastfinish=interval[0][1];
+        for (int i = 0; i < interval.length; i++) {
+            if (interval[i][0] > lastfinish) {
+                activityCount++;
+                lastfinish = interval[i][1];
+            }
+        }
+
+        return activityCount;
+
+    }
+}
