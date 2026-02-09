78555410a9f50ac17b3e5137ca474f66b0d8d4f6
mrinmoybm <eDr45gSc24#>
2026-02-09 09:01:21 +0600

    greedy approach

--- src/main/java/com/mricode/leetcode/dsa/greedy/GreedyApproach.java
+++ src/main/java/com/mricode/leetcode/dsa/greedy/GreedyApproach.java
@@ -50,4 +50,21 @@ public class GreedyApproach {
 
         return removed;
     }
+
+    //https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a, b) -> (a[1] <= b[1] ? -1 : 1));
+        int arrows = 1;
+        int lastEnd = points[0][1];
+         for (int point[] : points) {
+             //if there is no overlaping point , increase the arrow
+             if (point[0] > lastEnd) {
+                 arrows++;
+                 lastEnd = point[1];
+             }
+         }
+
+         return arrows;
+
+    }
 }
