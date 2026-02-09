b92e58e3f98c950f08bc04b3eaffa0dbd9dc05e4
mrinmoybm <eDr45gSc24#>
2026-02-09 08:37:26 +0600

    greedy approach

--- src/main/java/com/mricode/leetcode/dsa/greedy/GreedyApproach.java
+++ src/main/java/com/mricode/leetcode/dsa/greedy/GreedyApproach.java
@@ -30,4 +30,24 @@ public class GreedyApproach {
         return activityCount;
 
     }
+
+    //https://leetcode.com/problems/non-overlapping-intervals/description/
+    public int eraseOverlapIntervals(int[][] intervals) {
+        //[[s,e], [s,e]]
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int removed = 0;
+        int lastEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < lastEnd) {
+                //overlap
+                removed++;
+            }
+            else {
+                lastEnd = intervals[i][1];
+            }
+        }
+
+        return removed;
+    }
 }
