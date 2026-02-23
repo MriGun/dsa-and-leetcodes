6ed9a5079e927fd5ad06f3829d4dbce8cfadf8d2
mrinmoybm <eDr45gSc24#>
2026-02-23 23:49:14 +0600

    greedy approach

--- src/main/java/com/mricode/leetcode/dsa/greedy/GreedyApproach.java
+++ src/main/java/com/mricode/leetcode/dsa/greedy/GreedyApproach.java
@@ -287,4 +287,28 @@ public class GreedyApproach {
 
         return false;
     }
+
+    //https://leetcode.com/problems/jump-game-ii/
+    public int jump(int[] nums) {
+        int end = 0;
+        int minJump = 0;
+        int maxIndex = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n-1; i++) {
+
+            maxIndex =  Math.max(maxIndex, i + nums[i]);
+
+            if (i == end) {
+                minJump++;
+                end = maxIndex;
+            }
+
+            if (end >= n-1) {
+                break;
+            }
+
+        }
+        return minJump;
+    }
 }
